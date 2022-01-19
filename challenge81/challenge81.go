2046f27a5fe535eb120b3b4c81e15415b003aa7c
David Liebert <david.liebert@concepts.co.nz>
2022-01-20 10:58:49 +1300

    Much tidier version of Challenge 81 with pointers rather than silly indexing

--- challenge81/challenge81.go
+++ challenge81/challenge81.go
@@ -15,11 +15,14 @@ const (
 
 type Cell struct {
 	val    int
-	lChild int
-	rChild int
+	lChild *Cell
+	rChild *Cell
 }
 
-// These are some data for testing...
+// These are some data for testing.  If you want to use any of these
+// you will need to update the size constant above which reprents the
+// length of a row (or column) in the matrix.  And obviously change
+// the paramenter passed to descendTree() within Challenge81().
 //
 // var matrixValues = [sizeSquared]int{
 // 	131, 673, 234, 103, 18,
@@ -69,51 +72,64 @@ func loadMatrixFile() [sizeSquared]int {
 	return allValues
 }
 
-// Read the data as a simple series in an array.  From these create an array of cells
-// with each cell having a value and a reference number for a left and right child cell.
-// These child references all us to move down or right within the matrix.  Depending
-// on whether you think of the data as a matrix or a tree, "down" is also "left".
+// Read the data as a simple series in an array.  From these create an array of
+// cells with each cell having a value and pointers to left and right child cell.
+// These pointers to child cells allow us to move down or right within the matrix.
+// Depending on whether you think of the data as a matrix or a tree, "down" can
+// also be considered "left".
 func loadValues(matrix *[sizeSquared]Cell, matrixValues *[sizeSquared]int) {
 	for k, v := range *matrixValues {
 		matrix[k].val = v
 		if k%size != size-1 {
-			matrix[k].rChild = k + 1
+			matrix[k].rChild = &matrix[k+1]
 		} else {
-			matrix[k].rChild = -1
+			// There is no right child for the last cell in each matrix row
+			matrix[k].rChild = nil
 		}
 		if k/size != size-1 {
-			matrix[k].lChild = k + size
+			matrix[k].lChild = &matrix[k+size]
 		} else {
-			matrix[k].lChild = -1
+			// Cells in the last row in the matrix have no left child.
+			matrix[k].lChild = nil
 		}
 	}
 }
 
-func descendTree(t *[sizeSquared]Cell, n int) int {
+func descendTree(cell *Cell) int {
 	var lVal, rVal int
-	// If we are called with n == -1  this means there are no adjoing cells.
-	// Return a big number to ensure that any number in the data the will be preferred.
-	if n == -1 {
-		return reallyBig
+	// This is a recursive function with two special cases which return directly...
+
+	// Special case 1: if we are called with a nil pointer  this means there are no adjoining
+	// cells. Return a big number to ensure that any number in the data will be preferred.
+	if cell == nil {
+		return reallyBig // This is just a sentinel value
 	}
-	if t[n].lChild == -1 && t[n].rChild == -1 {
-		return t[n].val
+	// Special case 2: if we are at the lowest level of the matrix/triangle then both child
+	// pointers will be nil.  In this case just return the value (which triggers a recursive
+	// roll-up).
+	if cell.lChild == nil && cell.rChild == nil {
+		return cell.val
+	}
+	//  This is the non-special case which recurses...
+	lVal = descendTree(cell.lChild)
+	rVal = descendTree(cell.rChild)
+	// After getting the left and right values (which represent a sum of the lower level data
+	// to the left and right respectively) we add this sum to the current cell's value and
+	// then set lVal and rVal to nil so that those level cells can no longer be visited.
+	// Those cells are no longer relevant along this path because they have been accounted
+	// for.  Finally, return the sumary value.
+	if rVal < lVal {
+		cell.val += rVal
 	} else {
-		lVal = descendTree(t, t[n].lChild)
-		rVal = descendTree(t, t[n].rChild)
-		if rVal < lVal {
-			t[n].val += rVal
-		} else {
-			t[n].val += lVal
-		}
-		t[n].rChild = -1
-		t[n].lChild = -1
-		return t[n].val
+		cell.val += lVal
 	}
+	cell.rChild = nil
+	cell.lChild = nil
+	return cell.val
 }
 
 func Challenge81() {
 	rawValues := loadMatrixFile()
 	loadValues(&matrix, &rawValues)
-	fmt.Printf("Challenge 81 solution: %d\n", descendTree(&matrix, 0))
+	fmt.Printf("Challenge 81 solution: %d\n", descendTree(&matrix[0]))
 }
