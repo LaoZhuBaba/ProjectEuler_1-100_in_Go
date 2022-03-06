fb59e8ab2ab15e6f5bb8a8082c8de731da58365e
David Liebert <david.liebert@concepts.co.nz>
2022-03-06 20:22:30 +1300

    Solved 89

--- challenge89/challenge89.go
+++ challenge89/challenge89.go
@@ -1,9 +1,12 @@
 package challenge89
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 	"unicode"
 )
 
@@ -13,6 +16,20 @@ type Token struct {
 }
 
 type TokenSlice []Token
+
+type RomanCount struct {
+	symbol string
+	count  int
+	prefix string
+}
+
+func (rc RomanCount) String() string {
+	if rc.prefix != "" {
+		return strings.Repeat(rc.symbol, rc.count-1) + rc.prefix + rc.symbol
+	} else {
+		return strings.Repeat(rc.symbol, rc.count)
+	}
+}
 
 func newToken(r rune, count int) (Token, error) {
 	var t Token
@@ -88,6 +105,52 @@ func (t Token) tokenToInteger() int {
 	return t.value * t.count
 }
 
+func decToRoman(n int) string {
+
+	thousands := RomanCount{count: n / 1000, symbol: "M"}
+	five100s := RomanCount{count: n % 1000 / 500, symbol: "D"}
+	hundreds := RomanCount{count: n % 500 / 100, symbol: "C"}
+	fifties := RomanCount{count: n % 100 / 50, symbol: "L"}
+	tens := RomanCount{count: n % 50 / 10, symbol: "X"}
+	fives := RomanCount{count: n % 10 / 5, symbol: "V"}
+	units := RomanCount{count: n % 5, symbol: "I"}
+	if hundreds.count == 4 {
+		if five100s.count == 1 {
+			five100s.count = 0
+			thousands.count++
+			thousands.prefix = "C"
+		} else {
+			five100s.count = 1
+			five100s.prefix = "C"
+		}
+		hundreds.count = 0
+	}
+	if tens.count == 4 {
+		if fifties.count == 1 {
+			fifties.count = 0
+			hundreds.count++
+			hundreds.prefix = "X"
+		} else {
+			fifties.count = 1
+			fifties.prefix = "X"
+		}
+		tens.count = 0
+	}
+	if units.count == 4 {
+		if fives.count == 1 {
+			fives.count = 0
+			tens.count++
+			tens.prefix = "I"
+
+		} else {
+			fives.count = 1
+			fives.prefix = "I"
+		}
+		units.count = 0
+	}
+	return thousands.String() + five100s.String() + hundreds.String() + fifties.String() + tens.String() + fives.String() + units.String()
+}
+
 //func parseTokens(tokens TokenSlice, logger *log.Logger) int {
 func (tokens TokenSlice) parseTokens(logger *log.Logger) int {
 	// Iterate from first token to second-to-last token.  This means it is always
@@ -141,11 +204,43 @@ func (tokens TokenSlice) parseTokens(logger *log.Logger) int {
 	totalParseSum += tokens[last].tokenToInteger() - subtractor
 	return totalParseSum
 }
+
+func loadRoman(fileName string) (s []string, err error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file '%s': %w", fileName, err)
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		s = append(s, scanner.Text())
+	}
+	return s, err
+}
 func Challenge89() {
+	var originalTotalStrLen, finalTotalStrLen int
+	var max int
 	logger := log.New(os.Stderr, "", log.Lshortfile)
-	tokens := romNumTokenise("MMcDLxiv", logger)
-	//	logger.Printf("Tokens: %v\n", tokens)
+	tokens := romNumTokenise("MM", logger)
+	//logger.Printf("Tokens: %v\n", tokens)
 	sum := tokens.parseTokens(logger)
 	logger.Printf("%d\n", sum)
-
+	s, err := loadRoman("p089_roman.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var grandTotal int
+	for _, romNum := range s {
+		originalTotalStrLen += len(romNum)
+		tokens := romNumTokenise(romNum, logger)
+		sum := tokens.parseTokens(logger)
+		if sum > max {
+			max = sum
+		}
+		simplifiedRoman := decToRoman(sum)
+		finalTotalStrLen += len(simplifiedRoman)
+		logger.Printf("%s: %s: %d\n", romNum, simplifiedRoman, sum)
+		grandTotal += sum
+	}
+	logger.Printf("Grand Total: %d (max: %d)\n", grandTotal, max)
+	logger.Printf("Challenge 89 solution: %d\n", originalTotalStrLen-finalTotalStrLen)
 }
