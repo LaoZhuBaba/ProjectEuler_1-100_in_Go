bb0465b024f8087862e34295818e53e14a8f443c
David Liebert <david.liebert@kasna.com.au>
2023-09-02 11:47:05 +1200

    start of w/e

--- challenge93/challenge93.go
+++ challenge93/challenge93.go
@@ -2,6 +2,8 @@ package challenge93
 
 import (
 	"fmt"
+	"math"
+	"sort"
 )
 
 func setsWithinSet(n int) (sets [][]int) {
@@ -50,9 +52,99 @@ func nMemberSubsets2(subsetSize int, set []int) (subsets [][]int) {
 	return subsets
 }
 
+const (
+	ADD = iota
+	SUBTRACT
+	DIVIDE
+	MULTIPLY
+)
+
+func Arithmetic(parameters []int, operators []int) (result float64) {
+	result = float64(parameters[0])
+	for index, operator := range operators {
+		switch operator {
+		case ADD:
+			result += float64(parameters[index+1])
+
+		case SUBTRACT:
+			result -= float64(parameters[index+1])
+
+		case MULTIPLY:
+			result *= float64(parameters[index+1])
+
+		case DIVIDE:
+			result /= float64(parameters[index+1])
+		}
+	}
+	return result
+}
+
 func Challenge93() {
+	resultMap := make(map[string]map[float64]bool)
+	digitCombinations := nMemberSubsets(4, 9)
+	// fmt.Printf("digitCombinations: %v (length: %d)\n", digitCombinations, len(digitCombinations))
+	// ascending := [][]int{}
+	// for _, v := range digitCombinations {
+	// 	if v[0] < v[1] && v[1] < v[2] && v[2] < v[3] {
+	// 		v[0]++
+	// 		v[1]++
+	// 		v[2]++
+	// 		v[3]++
+	// 		ascending = append(ascending, v)
+	// 	}
+	// }
+	// fmt.Printf("ascending: %v (length: %d)\n", ascending, len(ascending))
+
 	result := AllOrderedCombinations(4)
 	fmt.Printf("result: %v (length: %d)\n", result, len(result))
+
+	for _, r := range result {
+		for _, c := range digitCombinations {
+			var sorted []int
+			var cc []int
+			cc = append(cc, c...)
+			cc[0]++
+			cc[1]++
+			cc[2]++
+			cc[3]++
+			sorted = append(sorted, cc...)
+			sort.Ints(sorted)
+			sortedStr := fmt.Sprintf("%v", sorted)
+			total := Arithmetic(cc, r)
+			if total < 1 {
+				continue
+			}
+			if math.Round(total) != total {
+				continue
+			}
+			if _, ok := resultMap[sortedStr]; !ok {
+				resultMap[sortedStr] = make(map[float64]bool)
+			}
+			if sortedStr == "[1 2 3 4]" {
+				fmt.Printf("cc is: %v with total %f and r: %d\n", cc, total, r)
+			}
+			resultMap[sortedStr][total] = true
+		}
+		// if math.Round(total) != total {
+		// 	continue
+		// }
+		//fmt.Printf("%f (%v)\n", total, r)
+		fmt.Printf("resultsMap['[1 2 3 4]']: %v\n", resultMap["[1 2 3 4]"])
+	}
+	// fmt.Printf("1 + 2 / 3 * 4 = %f\n", Arithmetic([]int{1, 2, 3, 4}, []int{ADD, DIVIDE, MULTIPLY}))
+	// fmt.Printf("5 + 2 / 4 - 3 = %f\n", Arithmetic([]int{5, 2, 4, 3}, []int{ADD, DIVIDE, SUBTRACT}))
+	// fmt.Printf("1 + 3 / 2 * 4 = %f\n", Arithmetic([]int{1, 3, 2, 4}, []int{ADD, DIVIDE, MULTIPLY}))
+	// fmt.Printf("1 + 4 / 2 - 6 = %f\n", Arithmetic([]int{1, 4, 2, 6}, []int{ADD, DIVIDE, SUBTRACT}))
+	// fmt.Printf("8 / 3 + 7 * 3 = %f\n", Arithmetic([]int{8, 3, 7, 3}, []int{DIVIDE, ADD, MULTIPLY}))
+	fmt.Printf("2 + 3 * 7 - 1 = %f\n", Arithmetic([]int{2, 3, 4, 1}, []int{ADD, MULTIPLY, SUBTRACT}))
+	f := Arithmetic([]int{8, 3, 7, 3}, []int{DIVIDE, ADD, MULTIPLY})
+	if math.Round(f) == f {
+		fmt.Printf("true!\n")
+	}
+	ordered := []int{2, 4, 3, 1}
+	sort.Ints(ordered)
+	orderedStr := fmt.Sprintf("%v", ordered)
+	fmt.Printf("orderedStr: %s\n", orderedStr)
 }
 
 // AllOrderedCombinations returns all possible ways that a set of things can be
@@ -64,7 +156,7 @@ func AllOrderedCombinations(max int) (result [][]int) {
 	// For our purposes the above means that a set of three things could
 	// contain 3 things of the same type, 3 different things or 2 + 1 in
 	// two different orders.
-	addends := setsWithinSet(max)
+	addends := setsWithinSet(max - 1)
 
 	// allSubsets will be a list of all possible ordered subsets.  E.g.,
 	// if max is 3 then all Subsets will be:
