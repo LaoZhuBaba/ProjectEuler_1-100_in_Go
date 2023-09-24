7d529dbe92774440ec58a52426ee01b72df0bc7e
David Liebert <david.liebert@kasna.com.au>
2023-09-25 10:17:30 +1300

    completed challenge 100

--- challenge100/challenge100.go
+++ challenge100/challenge100.go
@@ -2,36 +2,35 @@ package challenge100
 
 import (
 	"fmt"
+
+	"lukechampine.com/uint128"
 )
 
-func intDiviedBySqRootOf2(n uint64) uint64 {
-	return n * 470832 / 665857
+func intDiviedBySqRootOf2(n uint128.Uint128) uint128.Uint128 {
+	// 1607521/1136689 is a fractional approximation of the square root of 2
+	return n.Mul64(1136689).Div64(1607521)
 }
 func Challenge100() {
-	//gpf := shared.GetPrimeFactorsUint64
-	for denom1 := uint64(1); denom1 < 20_000_000_000; denom1++ {
-		denom2 := denom1 + 1
-		numer1 := intDiviedBySqRootOf2(denom1)
-		numer2 := numer1 + 1
-		// denom1Factors := gpf(denom1)
-		// denom2Factors := gpf(denom2)
-		// numer1Factors := gpf(numer1)
-		// numer2Factors := gpf(numer2)
 
-		if denom1*denom2 == numer1*numer2*2 {
-			fmt.Printf("%d/%d * %d/%d = 1/2\n", numer1, denom1, numer2, denom2)
-			fmt.Printf("%d - %d = %d\n", denom2, numer1, denom2-numer1)
-			// fmt.Printf("%v\n", denom1Factors)
-			// fmt.Printf("%v\n", denom2Factors)
-			// fmt.Printf("%v\n", numer1Factors)
-			// fmt.Printf("%v\n", numer2Factors)
+	max := uint128.From64(10_000_000_000_000)
+	var denom1, denom2, numer1, numer2 uint128.Uint128
+	// Experimentation shows that each successive solution increases by a factor of approx 5.82
+	for denom1 = uint128.From64(2); max.Cmp(denom1) == 1; denom1 = denom1.Mul64(582).Div64(100) {
+		for {
+			denom2 = denom1.Add64(1)
+			numer1 = intDiviedBySqRootOf2(denom1)
+			numer2 = numer1.Add64(1)
+			dd := denom1.Mul(denom2)
+			nn := numer1.Mul(numer2).Mul64(2)
+			if dd.Cmp(nn) == 0 {
+				// fmt.Printf("%d/%d * %d/%d = 1/2\n", numer1, denom1, numer2, denom2)
+				if denom2.Cmp64(999_999_999_999) == 1 {
+					fmt.Printf("challenge 100 solution is: %s\n", numer2.String())
+					return
+				}
+				break
+			}
+			denom1 = denom1.Add64(1)
 		}
 	}
 }
-
-// 20/14 * 21/15 = 420
-// 84/60 * 85/61
-
-// 84/119 * 85/120
-// 84/7 * 5/120
-// 7/7 * 5/10
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module euler
 
 go 1.16
 
-require github.com/stretchr/testify v1.8.4 // indirect
+require (
+	github.com/stretchr/testify v1.8.4 // indirect
+	lukechampine.com/uint128 v1.3.0 // indirect
+)
